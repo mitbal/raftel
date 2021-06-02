8d99567002be67949be63b9a4729733df53eb71b
M Iqbal Tawakal <mit.iqi@gmail.com>
2021-06-02 23:35:44 +0700

    bumped version to 0.2

--- tests/test_raftel.py
+++ tests/test_raftel.py
@@ -1,5 +1,14 @@
+import math
+
 from raftel import __version__
+from raftel.raftel import _rad_to_degree
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.2.0'
+
+
+def test_rad_conversion():
+    assert _rad_to_degree(0) == 0
+    assert _rad_to_degree(math.pi) == 180
+    assert _rad_to_degree(2*math.pi) == 360
